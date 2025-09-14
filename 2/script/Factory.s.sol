f1ad978fafd99054c2692a3a8ed720c621213ce5
juyeong-byeon <juyeongbyeon98@gmail.com>
2025-09-14 14:58:22 +0900

    feat: assginment 2 deployment

--- 2/script/Factory.s.sol
+++ 2/script/Factory.s.sol
@@ -18,10 +18,17 @@ contract FactoryScript is Script {
         vm.startBroadcast();
 
         // Step 1: Deploy MiniAMMFactory
-
+        miniAMMFactory = new MiniAMMFactory();
         // Step 2: Deploy two MockERC20 tokens
+        token0 = new MockERC20("MockToken1", "MCT1");
+        token1 = new MockERC20("MockToken2", "MCT2");
 
         // Step 3: Create a MiniAMM pair using the factory
+        address pairAddress = miniAMMFactory.createPair(address(token0), address(token1));
+        console.log("pair created", pairAddress);
+        address pairAddressCheck = miniAMMFactory.getPair(address(token0), address(token1));
+        console.log("get pair", pairAddressCheck);
+        require(pairAddress == pairAddressCheck, "should be same contract");
 
         vm.stopBroadcast();
     }
